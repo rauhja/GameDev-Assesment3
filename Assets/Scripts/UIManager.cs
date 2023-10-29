0a2daccf818c263103f74b96fdbc825413535dbb
rauhja <82282638+rauhja@users.noreply.github.com>
2023-10-29 22:00:20 +1100

    Add HUD items

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,15 +1,44 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    private static UIManager instance;
     private void Awake()
     {
-        DontDestroyOnLoad(gameObject);
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        SceneManager.sceneLoaded += OnSceneLoaded;
     }
 
-    public void LoadFirstLevel()
+    void LoadFirstLevel()
     {
         SceneManager.LoadScene(1);
+    }
+
+    void ExitScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.buildIndex == 0)
+        {
+            GameObject.FindGameObjectWithTag("Level1Button").GetComponent<Button>().onClick.AddListener(LoadFirstLevel);
+        }
+        if (scene.buildIndex == 1)
+        {
+            GameObject.FindGameObjectWithTag("ExitButton").GetComponent<Button>().onClick.AddListener(ExitScene);
+        }
     }
 }
